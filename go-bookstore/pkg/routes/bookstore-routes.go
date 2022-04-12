d71a49ebf1c46bb4c2bad7add006990bd6dfe357
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-12 21:43:24 +0100

    final commit

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/AnastasiiaDalakishvili/freecodecamp-course/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooK).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBoobById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
